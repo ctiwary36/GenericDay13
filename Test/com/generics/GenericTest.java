3ec1277dfcdb75359a74493b353c57f006705ab0
Chandan Tiwary <ctiwary36@gmail.com>
2021-07-15 01:24:15 +0530

    Added:UC3_3_String_Max

--- Test/com/generics/GenericTest.java
+++ Test/com/generics/GenericTest.java
@@ -29,4 +29,18 @@ class GenericsTest {
         float floatResult3 = generics.maxValueFloat(3.5f,2.7f,4.5f);
         assertEquals(4.5f,floatResult3);
     }
+
+    @Test
+    void maxString() {
+        String str1="Hii", str2 = "Hii Chandan" , str3 = "Hii Chandan How are you";
+
+        String stringResult1 = generics.maxString(str3,str2,str1);
+        assertEquals(str3,stringResult1);
+
+        String stringResult2 = generics.maxString(str2,str3,str1);
+        assertEquals(str3,stringResult2);
+
+        String stringResult3 = generics.maxString(str1,str2,str3);
+        assertEquals(str3,stringResult3);
+    }
 }
--- src/com/generics/Generics.java
+++ src/com/generics/Generics.java
@@ -21,6 +21,16 @@ public class Generics {
         return max;
     }
 
+    public String maxString(String str1, String str2, String str3){
+        String max = str1;
+        if (str2.compareTo(max) > 0 ){
+            max = str2;
+        } if (str3.compareTo(max) > 0 ){
+            max = str3;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("=======welcome to generics code tested By JUnit===== ");
         Generics generics = new Generics();
@@ -28,11 +38,12 @@ public class Generics {
         int result =  generics.maxValueInt(val1,val2,val3);
         System.out.println("the max vale of "+val1+" "+val2+" "+ val3+ " is :: "+ result);
 
-
         float float1 = 2.5f, float2 = 3.5f , float3 = 4.5f;
         float result1 = generics.maxValueFloat(float1,float2,float3);
         System.out.println("the max vale of "+float1+" , "+float2+" & "+ float3+ " is :: "+ result1);
 
-
+        String str1="Hii", str2 = "Hii Chandan" , str3 = "Hii Chandan How are you";
+        String result2 = generics.maxString(str1,str2,str3);
+        System.out.println("the max vale of ("+str1+") , ("+str2+") & ("+str3+ ") is :: "+ result2);
     }
 }
