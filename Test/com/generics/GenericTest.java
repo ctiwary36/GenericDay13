d78ae74111d20c524ac6dfaa17b47367ac159949
Chandan Tiwary <ctiwary36@gmail.com>
2021-07-15 01:29:59 +0530

    Added:UC4_extend_max_method_to_morethan_3_paraeter

--- Test/com/generics/GenericTest.java
+++ Test/com/generics/GenericTest.java
@@ -1,46 +1,46 @@
-package com.generics;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class GenericsTest {
-    Generics generics = new Generics();
-
-    @Test
-    void maxValue() {
-        int result1 = generics.maxValueInt(4,3,2);
-        assertEquals(4,result1);
-
-        int result2 = generics.maxValueInt(3,4,2);
-        assertEquals(4,result2);
-
-        int result3 = generics.maxValueInt(2,3,4);
-        assertEquals(4,result3);
-    }
-
-    @Test
-    void maxValueFloat() {
-        float floatResult1 = generics.maxValueFloat(4.5f,3.5f,2.7f);
-        assertEquals(4.5f,floatResult1);
-
-        float floatResult2 = generics.maxValueFloat(3.5f,4.5f,2.7f);
-        assertEquals(4.5f,floatResult2);
-
-        float floatResult3 = generics.maxValueFloat(3.5f,2.7f,4.5f);
-        assertEquals(4.5f,floatResult3);
-    }
-
-    @Test
-    void maxString() {
-        String str1="Hii", str2 = "Hii Chandan" , str3 = "Hii Chandan How are you";
-
-        String stringResult1 = generics.maxString(str3,str2,str1);
-        assertEquals(str3,stringResult1);
-
-        String stringResult2 = generics.maxString(str2,str3,str1);
-        assertEquals(str3,stringResult2);
-
-        String stringResult3 = generics.maxString(str1,str2,str3);
-        assertEquals(str3,stringResult3);
-    }
-}
+//package com.generics;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class GenericsTest {
+//    Generics generics = new Generics();
+//
+//    @Test
+//    void maxValue() {
+//        int result1 = generics.maxValueInt(4,3,2);
+//        assertEquals(4,result1);
+//
+//        int result2 = generics.maxValueInt(3,4,2);
+//        assertEquals(4,result2);
+//
+//        int result3 = generics.maxValueInt(2,3,4);
+//        assertEquals(4,result3);
+//    }
+//
+//    @Test
+//    void maxValueFloat() {
+//        float floatResult1 = generics.maxValueFloat(4.5f,3.5f,2.7f);
+//        assertEquals(4.5f,floatResult1);
+//
+//        float floatResult2 = generics.maxValueFloat(3.5f,4.5f,2.7f);
+//        assertEquals(4.5f,floatResult2);
+//
+//        float floatResult3 = generics.maxValueFloat(3.5f,2.7f,4.5f);
+//        assertEquals(4.5f,floatResult3);
+//    }
+//
+//    @Test
+//    void maxString() {
+//        String str1="Hii", str2 = "Hii Chandan" , str3 = "Hii Chandan How are you";
+//
+//        String stringResult1 = generics.maxString(str3,str2,str1);
+//        assertEquals(str3,stringResult1);
+//
+//        String stringResult2 = generics.maxString(str2,str3,str1);
+//        assertEquals(str3,stringResult2);
+//
+//        String stringResult3 = generics.maxString(str1,str2,str3);
+//        assertEquals(str3,stringResult3);
+//    }
+//}
--- src/com/generics/Generics.java
+++ src/com/generics/Generics.java
@@ -1,49 +1,24 @@
 package com.generics;
 
-public class Generics {
-    public int maxValueInt(int val1, int val2, int val3) {
-        int max = val1;
-        if (val2 > max) {
+public class Generics<T extends Comparable<T>> {
+    public static <T extends  Comparable <T>>  T maximumValue( T val1 , T val2, T val3 ,T val4,T val5) {
+        T max = val1;
+        if (val2.compareTo(max) > 0 ) {
             max = val2;
-        } if (val3 > max) {
+        } if (val3.compareTo(max) > 0){
             max = val3;
-        }
-        return max;
-    }
-
-    public float maxValueFloat(float val1, float val2, float val3){
-        float max = val1;
-        if (val2 > max) {
-            max = val2;
-        } if (val3 > max) {
-            max = val3;
-        }
-        return max;
-    }
-
-    public String maxString(String str1, String str2, String str3){
-        String max = str1;
-        if (str2.compareTo(max) > 0 ){
-            max = str2;
-        } if (str3.compareTo(max) > 0 ){
-            max = str3;
+        }if (val4.compareTo(max) > 0 ){
+            max = val4;
+        }if (val5.compareTo(max) > 0){
+            max = val5;
         }
         return max;
     }
 
     public static void main(String[] args) {
         System.out.println("=======welcome to generics code tested By JUnit===== ");
+
         Generics generics = new Generics();
-        int val1 = 2, val2 =3, val3 = 4;
-        int result =  generics.maxValueInt(val1,val2,val3);
-        System.out.println("the max vale of "+val1+" "+val2+" "+ val3+ " is :: "+ result);
-
-        float float1 = 2.5f, float2 = 3.5f , float3 = 4.5f;
-        float result1 = generics.maxValueFloat(float1,float2,float3);
-        System.out.println("the max vale of "+float1+" , "+float2+" & "+ float3+ " is :: "+ result1);
-
-        String str1="Hii", str2 = "Hii Chandan" , str3 = "Hii Chandan How are you";
-        String result2 = generics.maxString(str1,str2,str3);
-        System.out.println("the max vale of ("+str1+") , ("+str2+") & ("+str3+ ") is :: "+ result2);
+        System.out.println( "The maximum value is :: "+maximumValue(2.5f,3.5f,4.5f,5.5f,6.5f));
     }
 }
