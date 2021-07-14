dc1489f24bd94c7cd02884c6e5d0d436d01b694e
Chandan Tiwary <ctiwary36@gmail.com>
2021-07-15 01:14:35 +0530

    Added;UC2_1st_Position_return_same

--- Test/com/generics/GenericTest.java
+++ Test/com/generics/GenericTest.java
@@ -8,15 +8,25 @@ class GenericsTest {
 
     @Test
     void maxValue() {
-        int result1 = generics.maxValue(4,3,2);
+        int result1 = generics.maxValueInt(4,3,2);
         assertEquals(4,result1);
 
-        int result2 = generics.maxValue(3,4,2);
+        int result2 = generics.maxValueInt(3,4,2);
         assertEquals(4,result2);
 
-        int result3 = generics.maxValue(2,3,4);
+        int result3 = generics.maxValueInt(2,3,4);
         assertEquals(4,result3);
     }
 
+    @Test
+    void maxValueFloat() {
+        float floatResult1 = generics.maxValueFloat(4.5f,3.5f,2.7f);
+        assertEquals(4.5f,floatResult1);
 
+        float floatResult2 = generics.maxValueFloat(3.5f,4.5f,2.7f);
+        assertEquals(4.5f,floatResult2);
+
+        float floatResult3 = generics.maxValueFloat(3.5f,2.7f,4.5f);
+        assertEquals(4.5f,floatResult3);
+    }
 }
--- src/com/generics/Generics.java
+++ src/com/generics/Generics.java
@@ -1,21 +1,38 @@
 package com.generics;
 
 public class Generics {
-        public int maxValue(int val1, int val2, int val3) {
-            int max = val1;
-            if (val2 > max) {
-                max = val2;
-            } if (val3 > max) {
-                max = val3;
-            }
-            return max;
+    public int maxValueInt(int val1, int val2, int val3) {
+        int max = val1;
+        if (val2 > max) {
+            max = val2;
+        } if (val3 > max) {
+            max = val3;
         }
+        return max;
+    }
 
-        public static void main(String[] args) {
-            System.out.println("=======welcome to generics code tested By JUnit===== ");
-            Generics generics = new Generics();
-            int val1 = 2, val2 =3, val3 = 4;
-            int result =  generics.maxValue(val1,val2,val3);
-            System.out.println("the max vale of "+val1+" "+val2+" "+ val3+ " is :: "+ result);
-       }
+    public float maxValueFloat(float val1, float val2, float val3){
+        float max = val1;
+        if (val2 > max) {
+            max = val2;
+        } if (val3 > max) {
+            max = val3;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=======welcome to generics code tested By JUnit===== ");
+        Generics generics = new Generics();
+        int val1 = 2, val2 =3, val3 = 4;
+        int result =  generics.maxValueInt(val1,val2,val3);
+        System.out.println("the max vale of "+val1+" "+val2+" "+ val3+ " is :: "+ result);
+
+
+        float float1 = 2.5f, float2 = 3.5f , float3 = 4.5f;
+        float result1 = generics.maxValueFloat(float1,float2,float3);
+        System.out.println("the max vale of "+float1+" , "+float2+" & "+ float3+ " is :: "+ result1);
+
+
+    }
 }
