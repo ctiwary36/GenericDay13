84b7cb0670b4e61adced1c47de822772d139cbba
Chandan Tiwary <ctiwary36@gmail.com>
2021-07-15 01:37:55 +0530

    Added:UC5_Extend_maxMethod_And_call_test

--- src/com/generics/Generics.java
+++ src/com/generics/Generics.java
@@ -1,7 +1,22 @@
 package com.generics;
 
 public class Generics<T extends Comparable<T>> {
-    public static <T extends  Comparable <T>>  T maximumValue( T val1 , T val2, T val3 ,T val4,T val5) {
+
+    T val1,val2,val3,val4,val5;
+
+    public Generics(T val1, T val2, T val3, T val4, T val5) {
+        this.val1 = val1;
+        this.val2 = val2;
+        this.val3 = val3;
+        this.val4 = val4;
+        this.val5 = val5;
+    }
+    public T maximumValue(){
+        return maximumValue(val1,val2,val3,val4,val5);
+
+    }
+
+    public static <T extends  Comparable <T>>  T maximumValue(T val1 , T val2, T val3 , T val4, T val5) {
         T max = val1;
         if (val2.compareTo(max) > 0 ) {
             max = val2;
@@ -11,14 +26,21 @@ public class Generics<T extends Comparable<T>> {
             max = val4;
         }if (val5.compareTo(max) > 0){
             max = val5;
+
         }
+        printMax(val1,val2,val3,max);
         return max;
+    }
+
+    private static <T> void  printMax(T val1, T val2, T val3, T max) {
+        System.out.println("the maximum of" +val1+","+val2+" & " + val3+ " is :: " + max );
     }
 
     public static void main(String[] args) {
         System.out.println("=======welcome to generics code tested By JUnit===== ");
+        maximumValue(2,3,4,5,6);
+        maximumValue(3.5f,4.7f,1.2f,6.8f,5.5f);
+        new Generics<>(3,4,5,6,7).maximumValue();
 
-        Generics generics = new Generics();
-        System.out.println( "The maximum value is :: "+maximumValue(2.5f,3.5f,4.5f,5.5f,6.5f));
     }
 }
